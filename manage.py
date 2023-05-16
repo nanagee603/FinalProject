73a2d54dab875932f1a8a4b48cd5e5021b3fb022
nanagee603 <nanagee603@gmail.com>
2023-05-16 19:28:30 -0400

    Initial commit

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -15,19 +16,35 @@ def registration():
 # Define the employee information page route
 @app.route('/information')
 def information():
-    # Query the database to get all employees
-    # You can use any SQLite library for Python, such as sqlite3 or SQLAlchemy
-    employees = [(1, 'John', 'Male', '123-456-7890', '1990-01-01'),
-                 (2, 'Jane', 'Female', '123-456-7891', '1991-02-02'),
-                 (3, 'Bob', 'Male', '123-456-7892', '1992-03-03'),
-                 (4, 'Alice', 'Female', '123-456-7893', '1993-04-04'),
-                 (5, 'Jack', 'Male', '123-456-7894', '1994-05-05'),
-                 (6, 'Jill', 'Female', '123-456-7895', '1995-06-06'),
-                 (7, 'David', 'Male', '123-456-7896', '1996-07-07'),
-                 (8, 'Emily', 'Female', '123-456-7897', '1997-08-08'),
-                 (9, 'Mike', 'Male', '123-456-7898', '1998-09-09')]
+    # Establish a connection to the MySQL database
+    with mysql.connector.connect(
+        host="localhost",
+        user="flask",
+        password="ubuntu",
+        database="flask_db"
+    ) as con:
+        # Create a cursor object to execute SQL queries
+        cur = con.cursor()
+        
+        # Create a table if it doesn't exist
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS employees (
+                id INT PRIMARY KEY,
+                name VARCHAR(255),
+                gender VARCHAR(255),
+                phone VARCHAR(255),
+                birthdate DATE
+            )
+        """)
+        
+        # Query the database to get all employees
+        cur.execute("SELECT * FROM employees")
+        employees = cur.fetchall()
+    
     return render_template('information.html', employees=employees)
 
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000)
+
+
 
